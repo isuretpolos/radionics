672bb24c61ccb2b2ff2ef08abc6777606d30b3e0
isuretpolos <isuret.polos@gmail.com>
2023-04-12 19:12:18 +0200

    initial commit

---
+++ ui/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from "./components/app/app.component";
+import {HashLocationStrategy, LocationStrategy} from "@angular/common";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
